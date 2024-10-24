83d9bddf0c77af88a0b6bf2418ad59991eb02fc5
Emir Esengül <emiresgdev@gmail.com>
2024-10-24 11:54:12 +0300

    Finished the "Order Confrimation" Chapter

--- Bulky.Models/ViewModel/ShoppingCartVM.cs
+++ Bulky.Models/ViewModel/ShoppingCartVM.cs
@@ -9,7 +9,7 @@ namespace Bulky.Models.ViewModel
     public class ShoppingCartVM
     {
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
-
-        public double OrderTotal { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+        
     }
 }
